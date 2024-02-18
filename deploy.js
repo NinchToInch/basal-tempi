c0a9fb239429eba125c1f233eb35b05ecd6eb46c
NichToInch <ninch.schulz@gmail.com>
2024-02-18 18:50:27 +0100

    fixes

--- deploy.js
+++ deploy.js
@@ -6,6 +6,5 @@ const destDir = './docs';
 // Leere zuerst das Zielverzeichnis, kopiere dann die Dateien und lösche das Quellverzeichnis
 fs.emptyDir(destDir)
     .then(() => fs.copy(srcDir, destDir))
-    .then(() => fs.remove(srcDir)) // Lösche das /dist Verzeichnis
     .then(() => console.log('Deployment erfolgreich! Dateien wurden nach /docs verschoben und /dist wurde gelöscht.'))
     .catch(err => console.error('Deployment fehlgeschlagen:', err));
